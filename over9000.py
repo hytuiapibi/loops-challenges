2af7f5a4fccc046e51e1fbc3ff024af080e5473d
Anthony Batitto <ebatitto@gmail.com>
2023-08-18 23:56:54 -0400

    Update over9000.py
    Added comments

--- over9000.py
+++ over9000.py
@@ -1,11 +1,11 @@
-#Define the function to accept a list of numbers
-#Create a variable to keep track of our sum
-#Iterate through every element in our list of numbers
-#Within the loop, add the current number we are looking at to our sum
-#Still within the loop, check if the sum is greater than 9000. If it is, end the loop
-#Return the value of the sum when we ended our loop
+# Define the function to accept a list of numbers
+# Create a variable to keep track of our sum
+# Iterate through every element in our list of numbers
+# Within the loop, add the current number we are looking at to our sum
+# Still within the loop, check if the sum is greater than 9000. If it is, end the loop
+# Return the value of the sum when we ended our loop
 
-My solution:
+# My solution:
 
 def over_nine_thousand(lst):
   sum1 = 0
@@ -15,5 +15,5 @@ def over_nine_thousand(lst):
       break
   return sum1
 
-Test:
+# Test:
 print(over_nine_thousand([8000, 900, 120, 5000]))
